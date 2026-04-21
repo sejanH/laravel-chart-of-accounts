2330c72392d93a77f74da2000d5f43aa50fa98f1
S. M. Mominul Haque Sejan <sejan840@gmail.com>
2026-04-21 15:57:42 +0600

    added laravel-spatie-permissions permission middleware

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,55 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Sejan\Finance\Http\Controllers\AccountController;
+use Sejan\Finance\Http\Controllers\JournalEntryController;
+use Sejan\Finance\Http\Controllers\JournalEntryLineController;
+use Sejan\Finance\Http\Controllers\AccountingLedgerController;
 
-// Put API routes for Finance here, when needed.
+// Finance API Routes with permission middleware
+Route::middleware(['auth:sanctum'])->prefix('finance')->name('finance.api.')->group(function () {
+    // Accounts - Read (requires finance.read or finance.manage)
+    Route::middleware(['finance.permission:finance.read'])->group(function () {
+        Route::get('/accounts', [AccountController::class, 'index'])->name('accounts.index');
+        Route::get('/accounts/{account}', [AccountController::class, 'show'])->name('accounts.show');
+    });
+
+    // Accounts - Create/Update/Delete (requires finance.manage)
+    Route::middleware(['finance.permission:finance.manage'])->group(function () {
+        Route::post('/accounts', [AccountController::class, 'store'])->name('accounts.store');
+        Route::put('/accounts/{account}', [AccountController::class, 'update'])->name('accounts.update');
+        Route::delete('/accounts/{account}', [AccountController::class, 'destroy'])->name('accounts.destroy');
+    });
+
+    // Journal Entries - Read (requires finance.read or finance.manage)
+    Route::middleware(['finance.permission:finance.read'])->group(function () {
+        Route::get('/journal-entries', [JournalEntryController::class, 'index'])->name('journal-entries.index');
+        Route::get('/journal-entries/{journalEntry}', [JournalEntryController::class, 'show'])->name('journal-entries.show');
+    });
+
+    // Journal Entries - Create/Update/Delete (requires finance.manage)
+    Route::middleware(['finance.permission:finance.manage'])->group(function () {
+        Route::post('/journal-entries', [JournalEntryController::class, 'store'])->name('journal-entries.store');
+        Route::put('/journal-entries/{journalEntry}', [JournalEntryController::class, 'update'])->name('journal-entries.update');
+        Route::delete('/journal-entries/{journalEntry}', [JournalEntryController::class, 'destroy'])->name('journal-entries.destroy');
+    });
+
+    // Journal Entry Lines - Read (requires finance.read or finance.manage)
+    Route::middleware(['finance.permission:finance.read'])->group(function () {
+        Route::get('/journal-entry-lines', [JournalEntryLineController::class, 'index'])->name('journal-entry-lines.index');
+        Route::get('/journal-entry-lines/{journalEntryLine}', [JournalEntryLineController::class, 'show'])->name('journal-entry-lines.show');
+    });
+
+    // Journal Entry Lines - Create/Update/Delete (requires finance.manage)
+    Route::middleware(['finance.permission:finance.manage'])->group(function () {
+        Route::post('/journal-entry-lines', [JournalEntryLineController::class, 'store'])->name('journal-entry-lines.store');
+        Route::put('/journal-entry-lines/{journalEntryLine}', [JournalEntryLineController::class, 'update'])->name('journal-entry-lines.update');
+        Route::delete('/journal-entry-lines/{journalEntryLine}', [JournalEntryLineController::class, 'destroy'])->name('journal-entry-lines.destroy');
+    });
+
+    // Accounting Ledger - Read (requires finance.read or finance.manage)
+    Route::middleware(['finance.permission:finance.read'])->group(function () {
+        Route::get('/accounting-ledger', [AccountingLedgerController::class, 'index'])->name('ledger.index');
+    });
+});
+});
--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,26 @@ use Sejan\Finance\Http\Controllers\VoucherWorkflowController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web', 'auth'])->prefix('finance')->name('finance.')->group(function () {
-    // Voucher Workflow
-    Route::get('/voucher-entry', [VoucherWorkflowController::class, 'index'])->name('vouchers.index');
-    Route::get('/voucher-entry/{voucher}/details', [VoucherWorkflowController::class, 'details'])->name('vouchers.details');
-    Route::get('/voucher-entry/{voucher}/print', [VoucherPrintController::class, 'print'])->name('vouchers.print');
-    Route::get('/voucher-entry/{voucher}/pdf', [VoucherPrintController::class, 'pdf'])->name('vouchers.pdf');
-    Route::get('/chart-of-accounts', [VoucherAccountController::class, 'index'])->name('chart-of-accounts.index');
+    // Voucher Workflow - Read/Export (requires finance.read or finance.manage)
+    Route::middleware(['finance.permission:finance.read'])->group(function () {
+        Route::get('/voucher-entry', [VoucherWorkflowController::class, 'index'])->name('vouchers.index');
+        Route::get('/voucher-entry/{voucher}/details', [VoucherWorkflowController::class, 'details'])->name('vouchers.details');
+        Route::get('/voucher-entry/{voucher}/print', [VoucherPrintController::class, 'print'])->name('vouchers.print');
+        Route::get('/voucher-entry/{voucher}/pdf', [VoucherPrintController::class, 'pdf'])->name('vouchers.pdf');
+        Route::get('/chart-of-accounts', [VoucherAccountController::class, 'index'])->name('chart-of-accounts.index');
+    });
 
-    // Store/Update/Delete
-    Route::post('/voucher-entry', [VoucherWorkflowController::class, 'store'])->name('vouchers.store');
-    Route::put('/voucher-entry/{voucher}', [VoucherWorkflowController::class, 'update'])->name('vouchers.update');
-    Route::delete('/voucher-entry/{voucher}', [VoucherWorkflowController::class, 'destroy'])->name('vouchers.destroy');
-    Route::post('/voucher-entry/{voucher}/details', [VoucherWorkflowController::class, 'storeLine'])->name('vouchers.lines.store');
-    Route::put('/voucher-entry/{voucher}/details/{voucherLine}', [VoucherWorkflowController::class, 'updateLine'])->name('vouchers.lines.update');
-    Route::delete('/voucher-entry/{voucher}/details/{voucherLine}', [VoucherWorkflowController::class, 'destroyLine'])->name('vouchers.lines.destroy');
+    // Store/Update/Delete (requires finance.manage)
+    Route::middleware(['finance.permission:finance.manage'])->group(function () {
+        Route::post('/voucher-entry', [VoucherWorkflowController::class, 'store'])->name('vouchers.store');
+        Route::put('/voucher-entry/{voucher}', [VoucherWorkflowController::class, 'update'])->name('vouchers.update');
+        Route::delete('/voucher-entry/{voucher}', [VoucherWorkflowController::class, 'destroy'])->name('vouchers.destroy');
+        Route::post('/voucher-entry/{voucher}/details', [VoucherWorkflowController::class, 'storeLine'])->name('vouchers.lines.store');
+        Route::put('/voucher-entry/{voucher}/details/{voucherLine}', [VoucherWorkflowController::class, 'updateLine'])->name('vouchers.lines.update');
+        Route::delete('/voucher-entry/{voucher}/details/{voucherLine}', [VoucherWorkflowController::class, 'destroyLine'])->name('vouchers.lines.destroy');
 
-    Route::post('/chart-of-accounts', [VoucherAccountController::class, 'store'])->name('chart-of-accounts.store');
-    Route::put('/chart-of-accounts/{voucherAccount}', [VoucherAccountController::class, 'update'])->name('chart-of-accounts.update');
-    Route::delete('/chart-of-accounts/{voucherAccount}', [VoucherAccountController::class, 'destroy'])->name('chart-of-accounts.destroy');
+        Route::post('/chart-of-accounts', [VoucherAccountController::class, 'store'])->name('chart-of-accounts.store');
+        Route::put('/chart-of-accounts/{voucherAccount}', [VoucherAccountController::class, 'update'])->name('chart-of-accounts.update');
+        Route::delete('/chart-of-accounts/{voucherAccount}', [VoucherAccountController::class, 'destroy'])->name('chart-of-accounts.destroy');
+    });
 });
